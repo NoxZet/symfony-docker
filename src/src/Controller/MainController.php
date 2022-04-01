163f05071645e014ea68b8c733a38ee90febba87
NoxZet <nicolas.m@email.cz>
2022-04-01 16:51:42 +0200

    Add Twig

--- src/src/Controller/MainController.php
+++ src/src/Controller/MainController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
 	#[Route('/')]
 	public function main(): Response {
-		return new Response('');
+		return $this->render('Main/main.html.twig');
 	}
 }
