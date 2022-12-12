83d275725fb529ce61d1593dae445e1f7a2543c9
NoxZet <nicolas.m@email.cz>
2022-12-12 19:56:22 +0100

    Adjust User properties, login and register forms

---
+++ src/src/Model/Form/LoginType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Model\Form;
+
+use App\Model\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options): void {
+		$builder
+			->add('email', TextType::class, ['label' => 'Email'])
+			->add('password', TextType::class, ['label' => 'Password'])
+			->add('submit', SubmitType::class, ['label' => 'Sign in']);
+	}
+}
--- src/src/Model/Form/RegisterType.php
+++ src/src/Model/Form/RegisterType.php
@@ -9,19 +9,12 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class RegisterType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options): void {
 		$builder
-			->add('firstName', TextType::class, ['label' => 'Jméno'])
-			->add('lastName', TextType::class, ['label' => 'Příjmení'])
-			->add('phone', TextType::class, ['label' => 'Telefon'])
-			->add('insert', SubmitType::class, ['label' => 'Vložit']);
-	}
-
-	public function configureOptions(OptionsResolver $resolver) {
-		$resolver->setDefaults([
-			'data_class' => User::class,
-		]);
+			->add('email', TextType::class, ['label' => 'Email'])
+			->add('password', TextType::class, ['label' => 'Password'])
+			->add('submit', SubmitType::class, ['label' => 'Create account']);
 	}
 }
